5492f1387511f09862235be51507bbca9a5b0ea3
Naveen20181189 <146409923+Naveen20181189@users.noreply.github.com>
2024-08-30 18:30:40 +0530

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const window = require('window')
+
 require('dotenv').config();
 const mongoose = require('mongoose')
 const multer = require('multer')
